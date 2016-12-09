1cd54d32d92deb3a04537eb55dceb2a4d65773ce
Liz Tiller <elizabeth.tiller@gmail.com>
2016-12-08 21:33:06 -0500

    fucked up

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 
 class App extends Component {
 
+  faces = ['👹', '😱']
+// faces = ['👹', '😱']
+// matched = []
+// picks = []
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <h1>Memory!</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+        </tbody>
+      </table>
     </div>
   }
 }
 
 export default App
+// https://gist.github.com/ambethia/e9bd1760f78fe36dcd7e03a1c2a4edc1
